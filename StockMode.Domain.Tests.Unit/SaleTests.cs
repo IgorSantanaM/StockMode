e8fa35ad29dd0fba36ed282daa5204108bb709af
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2025-07-24 22:08:53 -0400

    (feat): Added Sale tests

---
+++ StockMode.Domain.Tests.Unit/SaleTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using StockMode.Domain.Enums;
+using StockMode.Domain.Sales;
+
+namespace StockMode.Domain.Tests.Unit
+{
+    public class SaleTests
+    {
+        [Fact]
+        public void CreateSale_ShouldSucceed_WhenPaymentMethodIsValid()
+        {
+            // Arrange 
+            var sale = new Sale(PaymentMethod.Pix);
+
+            // Assert
+            sale.Should().NotBeNull();
+            sale.PaymentMethod.Should().Be(PaymentMethod.Pix);
+            sale.SaleDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+        }
+
+
+    }
+}
