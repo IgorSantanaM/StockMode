1f55176e273df4ad03042c340a53608109436ecf
Igor <ra200246@ucdb.br>
2025-08-11 10:34:08 -0400

    (feat): Added customer and supplier relationship

---
+++ src/StockMode.Infra.Data/Migrations/20250811142244_Added Customer and supplier relationship.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockMode.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCustomerandsupplierrelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "StockMovements",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SupplierId",
+                table: "StockMovements",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "StockMovements");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierId",
+                table: "StockMovements");
+        }
+    }
+}
