cb923af2a23e5d30d8b122848d35d46d57c53616
Igor Santana Medeiros <1434@ucdb.br>
2025-11-25 14:52:30 -0400

    (feat): Added Integration test

---
+++ tests/StockMode.Presentation.WebApi.Tests.Integration/Endpoints/CustomerEndpoints/GetAllCustomersEnpointTest.cs
@@ -0,0 +1,55 @@
+﻿using Bogus;
+using FluentAssertions;
+using StockMode.Application.Common.Dtos;
+using StockMode.Application.Features.Customers.Commands.CreateCustomer;
+using StockMode.Application.Features.Customers.Dtos;
+using StockMode.Domain.Common;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace StockMode.Presentation.WebApi.Tests.Integration.Endpoints.CustomerEndpoints
+{
+    public class GetAllCustomersEnpointTest : IClassFixture<StockModeApiFactory>
+    {
+        private readonly HttpClient _client;
+        private readonly Faker<CreateCustomerCommand> _customerGenerator = new Faker<CreateCustomerCommand>()
+            .CustomInstantiator(faker => new CreateCustomerCommand(
+                faker.Person.FullName,
+                faker.Person.Email,
+                "(67) 99215-8961",
+                new AddressDto
+                (
+                    faker.Random.Int(0, 1000),
+                    faker.Address.StreetName(),
+                    faker.Address.City(),
+                    faker.Address.State(),
+                    faker.Address.ZipCode()
+                ),
+                null,
+                faker.Lorem.Sentence()
+                ));
+           
+
+        public GetAllCustomersEnpointTest(StockModeApiFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAllCustomers_ShouldReturnAllCustomers_WhenAllParametersAreValid()
+        {
+            // Arrange
+            var customer = _customerGenerator.Generate();
+            var createResponse = await _client.PostAsJsonAsync("/api/customers", customer);
+            
+            // Act
+            var response = await _client.GetAsync("/api/customers?page=1&pageSize=10");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Should().NotBeNull();
+            var customers = await response.Content.ReadFromJsonAsync<PagedResult<CustomerSummaryDto>>();
+            customers!.Items.Should().NotBeNull();
+        }
+    }
+}
---
+++ tests/StockMode.Presentation.WebApi.Tests.Integration/StockModeApiFactory.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using StockMode.Infra.Data.Contexts;
+using StockMode.Infra.Data.Factories;
+using StockMode.WebApi;
+using System.Data;
+using Testcontainers.PostgreSql;
+
+namespace StockMode.Presentation.WebApi.Tests.Integration
+{
+    public class StockModeApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
+    {
+
+        private readonly PostgreSqlContainer _dbContainer =
+             new PostgreSqlBuilder()
+                .WithDatabase("stockmode_test_db")
+                .WithUsername("postgres")
+                .WithPassword("postgrespw")
+                .WithImage("postgres:15-alpine")
+                .WithPortBinding(5432, 5432)
+                .Build();
+
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+            });
+
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<StockModeContext>));
+                
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<StockModeContext>(options =>
+                {
+                    options.UseNpgsql(_dbContainer.GetConnectionString());
+                });
+            });
+        }
+        
+        public async Task InitializeAsync()
+        {
+            await _dbContainer.StartAsync();
+            
+            using var scope = Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<StockModeContext>();
+            await context.Database.MigrateAsync();
+        }
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await _dbContainer.StopAsync();
+            await _dbContainer.DisposeAsync();
+        }
+    }
+}
